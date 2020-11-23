2fbfc95b72f310da106c0264db2fbceb336cb858
Andrew Eames <ajerxs@gmail.com>
2020-11-23 20:59:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,23 +2,20 @@ class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
   @@genre = []
+  @@artists = []
 
   def initialize(name, artist, genre)
     @@count += 1
     @name = name
     @artist = artist
+    @genre = genre
+    @@genre << @genre
+    @@artists << @artist
   end
 
   def count
     @@count
   end
 
-  def genre(genre)
-    @genre = genre
-    @@genre << @genre
-  end
-
-  def artists
-  end
 
 end
